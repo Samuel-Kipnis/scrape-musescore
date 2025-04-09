a2307889e6dd1c00862aa3fb0ce2e590802764b0
Samuel Kipnis <sam.kipnis@gmail.com>
2025-04-08 22:32:30 -0400

    e

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
-from seleniumbase import Driver
-from dotenv import load_dotenv
-# initialize driver in GUI mode with UC enabled
-driver = Driver(uc=True, headless=False)
+from start_driver import start_driver
+from download_score import download_score
+from login import login
+import time
 
-# set the target URL
-url = "https://www.scrapingcourse.com/cloudflare-challenge"
 
-# open URL using UC mode with 6 second reconnect time
-driver.uc_open_with_reconnect(url, reconnect_time=6)
+def main():
+    driver = start_driver()
+    login(driver)
+
+    url = "https://musescore.com/user/6662591/scores/4383881"
+    download_score(driver, url)
+    time.sleep(50)  # Wait for the download to finish
+
+
+if __name__ == "__main__":
+    main()
