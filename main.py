b1435083b19e106fbac4ebcb120e7132393b7050
Samuel Kipnis <sam.kipnis@gmail.com>
2025-04-08 22:32:52 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from start_driver import start_driver
-from download_score import download_score
 from login import login
 import time
 
@@ -7,10 +6,6 @@ import time
 def main():
     driver = start_driver()
     login(driver)
-
-    url = "https://musescore.com/user/6662591/scores/4383881"
-    download_score(driver, url)
-    time.sleep(50)  # Wait for the download to finish
 
 
 if __name__ == "__main__":
